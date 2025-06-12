c9da9615cfc52784918cd3f6327ac61af481b15d
Vanesline <haiduc159205.hsr@outlook.com>
2025-06-12 12:19:14 +0700

    v22

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934fabff00c99cb5e4ce3cb250777a77e5b2c00a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96092f7975b1704bc9b542cc701dcd5273f16d3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
